3fbb3eef7cdd095c169369b6603780962f174e06
Luke Melia <luke@lukemelia.com>
2013-07-09 18:59:54 -0400

    Make auth card use data service

--- cards/auth/card.js
+++ cards/auth/card.js
@@ -1,5 +1,6 @@
 import Conductor from 'conductor';
 import AuthenticatedStackoverflowApiConsumer from 'app/consumers/authenticated_stackoverflow_api';
+var RSVP = Conductor.Oasis.RSVP;
 
 Conductor.require('/vendor/jquery.js');
 Conductor.requireCSS('/cards/stackoverflow-auth/card.css');
@@ -8,18 +9,19 @@ var card = Conductor.card({
   consumers: {
     'oauth': Conductor.Oasis.Consumer.extend({
       getAccessTokenPromise: function(){
-        var service = this;
-        return card.consumers.userStorage.request('getItem', 'stackOverflowAccessToken').then(function(accessToken) {
-          if (accessToken) return accessToken;
+        // TODO: Use paneTypeUserEntries and paneTypeUserStorage instead
+        var accessToken = card.data.paneUserEntries.stackOverflowAccessToken;
+        if (accessToken) {
+          return RSVP.resolve(accessToken);
+        }
 
-          var url = 'https://stackexchange.com/oauth/dialog';
-          return service.request('authorize', {
-            authorizeUrl: url
-          }).then(function(accessToken) {
-            // TODO this token expires...
-            card.consumers.userStorage.request('setItem', 'stackOverflowAccessToken', accessToken).then(undefined, Conductor.error);
-            return accessToken;
-          });
+        var url = 'https://stackexchange.com/oauth/dialog';
+        return this.request('authorize', {
+          authorizeUrl: url
+        }).then(function(accessToken) {
+          // TODO this token expires...
+          card.consumers.userStorage.request('setItem', 'stackOverflowAccessToken', accessToken).then(undefined, Conductor.error);
+          return accessToken;
         });
       }
     }),
